3b5d225fc391912bcbdcbe4af5d4dda9d7d00b51
md-bappi <mdabuanasbappy555@gmail.com>
2024-02-28 09:35:40 +0600

    ui drafted

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,25 +1,108 @@
 import { currencyFormatter } from "../utilities/currencyFormatter";
 
-const Cart = ({ product }) => {
+const data = [
+  {
+    id: 1,
+    name: "Blink Mini - Compact indoor plug-in smart security camera",
+    description:
+      "Monitor the inside of your home day and night with our 1080P HD indoor plug-in smart security camera",
+    price: 64.99,
+    image:
+      "https://res.cloudinary.com/dy28teazb/image/upload/v1668172648/React%20Shopping/Products/81-585-013-01_a04wkd.jpg",
+    category: "Camera",
+  },
+  {
+    id: 2,
+    name: "Vlogging Camera, 4K Digital Camera for YouTube with WiFi",
+    description:
+      "It's super suitable for the 'happy snapper' who just hope to point and shoot to take good quality images",
+    price: 109.99,
+    image:
+      "https://res.cloudinary.com/dy28teazb/image/upload/v1668172649/React%20Shopping/Products/81pgsjFGpmL_qundpd.jpg",
+    category: "Camera",
+  },
+  {
+    id: 3,
+    name: "SAMSUNG 55-Inch Class Crystal 4K UHD AU8000 Series HDR",
+    description:
+      "Witness millions of shades of color through powerful Dynamic Crystal technology",
+    price: 497.99,
+    image:
+      "https://res.cloudinary.com/dy28teazb/image/upload/v1668172649/React%20Shopping/Products/cl-uhd-tu7090-un50tu7090gxzs-rperspective-285965740_duusj5.png",
+    category: "TV",
+  },
+];
+
+const Cart = () => {
   return (
-    <div className="product flex flex-col gap-2 bg-white shadow-md rounded-xl overflow-hidden hover:shadow-xl  duration-300">
-      <div className="img">
-        <img src={product.image} alt={product.name} />
+    <div className="cart-section container mx-auto py-10">
+      <h2 className="section-title uppercase text-2xl font-bold text-center mb-10">
+        Your cart
+      </h2>
+      <div className="cart-container">
+        <div className="product-headlines grid grid-cols-5  gap-10 border-b pb-3 uppercase font-medium">
+          <div className="col-product col-span-2">Product</div>
+          <div className="col-unit-price">Unit Price</div>
+          <div className="col-quantity">Quantity</div>
+          <div className="col-total-price ml-auto">Total Price</div>
+        </div>
+        <div className="products flex flex-col">
+          {data.map((product) => (
+            <div className="product grid grid-cols-5 gap-10 mt-10 border-b pb-5">
+              <div className="left flex col-span-2 gap-5">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="h-32 w-32 object-cover"
+                />
+                <div className="details flex flex-col gap-3 items-start">
+                  <span>{product.name}</span>
+                  <button className=" uppercase text-gray-400 hover:text-rose-500 duration-300">
+                    Remove
+                  </button>
+                </div>
+              </div>
+              <div className="unit-price">
+                {currencyFormatter(product.price)}
+              </div>
+              <div className="counter flex">
+                <button className="h-10 w-10 bg-gray-100 border border-gray-300 active:bg-gray-700 active:text-gray-50">
+                  -
+                </button>
+                <span className="h-10 w-10 bg-gray-100 flex justify-center items-center border border-gray-300">
+                  1
+                </span>
+                <button className="h-10 w-10 bg-gray-100 border border-gray-300 active:bg-gray-700 active:text-gray-50">
+                  +
+                </button>
+              </div>
+              <div className="total-price ml-auto">
+                {currencyFormatter(product.price)}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="texts flex flex-col gap-2 px-5 pb-5">
-        <span className="category-tag uppercase text-xs font-semibold tracking-widest text-teal-500">
-          {product.category}
-        </span>
-        <h3 className="title text-xl font-medium h-[5.25rem]">
-          {product.name}
-        </h3>
-        <p className="details text-gray-500 h-[6rem]">{product.description}</p>
-        <div className="flex justify-between items-center">
-          <span className="price text-xl font-medium text-rose-5001">
-            {currencyFormatter(product.price)}
-          </span>
-          <button className=" uppercase bg-violet-500 text-violet-50 font-medium py-3 px-8 rounded-md hover:bg-orange-500 hover:text-orange-50 duration-300 shadow-lg shadow-violet-300 hover:shadow-orange-300">
-            Add to cart
+      <div className="cart-lower flex justify-between items-start py-10">
+        <button
+          className="clear-btn uppercase border py-3 px-8
+        hover:bg-rose-200 hover:text-rose-600 font-medium hover:border-rose-200 duration-300"
+        >
+          clear cart
+        </button>
+        <div className="flex flex-col items-start gap-2">
+          <div className="top flex justify-between w-full text-2xl font-medium">
+            <span className="text-sky-500">Subtotal</span>
+            <span className="text-rose-500">$200</span>
+          </div>
+          <p className="text-gray-400">
+            Taxes and shopping costs are calculated at the checkout{" "}
+          </p>
+          <button className="checkout bg-sky-500 w-full py-3 uppercase font-medium text-sky-50 tracking-widest hover:bg-sky-600 duration-300">
+            Checkout
+          </button>
+          <button className="continue uppercase text-sky-500 font-medium">
+            Continue Shopping
           </button>
         </div>
       </div>
